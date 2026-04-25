b4f5d5dc21a00869af06d0e09562bacfd9786ab3
Yassine-Abouayyoub <yasynabwaywb3@gmail.com>
2026-04-25 19:26:53 +0100

    modification a page de presentation de mini projet

--- serie3/EX6 (mini projet)/script.js
+++ serie3/EX6 (mini projet)/script.js
@@ -1,15 +1,21 @@
-function afficherHeure() {
-    const maintenant = new Date();
+﻿function afficherHeure() {
+    const horloge = document.getElementById("horloge");
 
-    const date = maintenant.toLocaleDateString();
+    if(horloge !== null){
+        const maintenant = new Date();
 
-    const heure = maintenant.toLocaleTimeString();
+        const date = maintenant.toLocaleDateString();
 
-    document.getElementById("horloge").innerText = date + " - " + heure;
+        const heure = maintenant.toLocaleTimeString();
+
+        horloge.innerText = date + " - " + heure;
+    }
 }
 
-setInterval(afficherHeure, 1000);
-afficherHeure();
+if(document.getElementById("horloge") !== null){
+    setInterval(afficherHeure, 1000);
+    afficherHeure();
+}
 
 let dark = false;
 function darkmode(){
@@ -27,4 +33,59 @@ function darkmode(){
     }
 }
 
-alert("Bienvenue dans ma page web");
+if(document.body.className === "index"){
+    alert("Bienvenue dans ma page web");
+}
+function afficherMasquer(){
+    const contenu = document.getElementById("contenu");
+    const bouton = document.getElementById("btn-contenu");
+
+    if(contenu.style.display === "none"){
+        contenu.style.display = "block";
+        bouton.innerText = "Masquer le contenu";
+    }
+    else{
+        contenu.style.display = "none";
+        bouton.innerText = "Afficher le contenu";
+    }
+}
+function afficherDescription(){
+    const suite = document.getElementById("suite-description");
+    const bouton = document.getElementById("btn-description");
+
+    if(suite.style.display === "none"){
+        suite.style.display = "inline";
+        bouton.innerText = "Afficher moins";
+    }
+    else{
+        suite.style.display = "none";
+        bouton.innerText = "Afficher plus";
+    }
+}
+
+function changerCouleurTexte(){
+    const description = document.getElementById("description");
+
+    if(description.style.color === "red"){
+        description.style.color = "black";
+    }
+    else{
+        description.style.color = "red";
+    }
+}
+
+function survolCompetences(){
+    const competences = document.getElementById("competences");
+
+    competences.style.backgroundColor = "lightblue";
+    competences.style.color = "blue";
+}
+
+function quitterSurvol(){
+    const competences = document.getElementById("competences");
+
+    competences.style.backgroundColor = "white";
+    competences.style.color = "black";
+}
+
+
